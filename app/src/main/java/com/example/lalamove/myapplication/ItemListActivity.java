98200eb6db3d27755ea1a1ddd96dd01973c02c76
agathaflc <agatha.flc@hotmail.com>
2016-06-06 18:41:10 +0800

    stole some stuff from all the rages

--- app/src/main/java/com/example/lalamove/myapplication/ItemListActivity.java
+++ app/src/main/java/com/example/lalamove/myapplication/ItemListActivity.java
@@ -10,6 +10,7 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -92,6 +93,8 @@ public class ItemListActivity extends AppCompatActivity {
             holder.mItem = mValues.get(position);
             holder.mIdView.setText(mValues.get(position).id);
             holder.mContentView.setText(mValues.get(position).content);
+            //System.out.print("asdfg " + mValues.get(position).content); // try printing out
+            Log.i("asdfg", mValues.get(position).content);
 
             holder.mView.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -115,8 +118,8 @@ public class ItemListActivity extends AppCompatActivity {
             });
         }
 
-        @Override
-        public int getItemCount() {
+        //@Override
+        public int getIteCount() {
             return mValues.size();
         }
 
